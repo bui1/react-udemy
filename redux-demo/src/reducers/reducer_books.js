72f31224b640eeee6942b9cb4e43a0d326c5333e
Monica Bui <bui1@ualberta.ca>
2018-07-30 13:37:52 -0600

    redux container review using booklist example

---
+++ redux-demo/src/reducers/reducer_books.js
@@ -0,0 +1,11 @@
+export default function() {
+    return [
+        {title: "JS"},
+        {title: "JS2"},
+        {title: "JS3"},
+        {title: "JS4"}
+    ];
+};
+
+// create reducer
+// wire reducer with app
